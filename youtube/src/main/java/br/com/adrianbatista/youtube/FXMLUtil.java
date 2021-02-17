43c57835ff0c8f5fcf01f0755c8701673f27fb7c
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-02-16 22:52:42 -0300

    tratando Excessões

--- youtube/src/main/java/br/com/adrianbatista/youtube/FXMLUtil.java
+++ youtube/src/main/java/br/com/adrianbatista/youtube/FXMLUtil.java
@@ -4,19 +4,25 @@ import java.io.IOException;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 
 public class FXMLUtil {
-	
+
 	public static Scene loadScene(String fxml) {
 		try {
 			FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
 			Scene scene = new Scene(fxmlLoader.load());
 			return scene;
-		}catch (IOException e) {
-			System.err.println("Failed to load " + fxml + ".fxml");
+		} catch (IOException eIO) {
+			Alert alert = ExceptionUtil.error("Erro", "Erro ao carregar um componente", "Ao tentar carregar a janela" + fxml, eIO);
+			alert.showAndWait();
+			return null;
+		} catch (IllegalStateException eIllegalState) {
+			Alert alert = ExceptionUtil.error("Erro", "Erro arquivo inexistente", "Ao tentar carregar a janela" + fxml, eIllegalState);
+			alert.showAndWait();
 			return null;
 		}
-		
+
 	}
 
 }
