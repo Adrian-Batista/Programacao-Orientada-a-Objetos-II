c88fd0329965cf7825957e87e15e62cabb687f00
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-02-16 21:59:33 -0300

    Navegando telas

--- youtube/src/main/java/br/com/adrianbatista/youtube/LoginController.java
+++ youtube/src/main/java/br/com/adrianbatista/youtube/LoginController.java
@@ -3,7 +3,10 @@ package br.com.adrianbatista.youtube;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class LoginController {
 	@FXML
@@ -17,6 +20,20 @@ public class LoginController {
 			System.err.println("Erro ao carregar a Janela Principal!");
 		}
 			
+	}
+	
+	@FXML
+	private void register() {
+        try {
+        	Stage stage = new Stage();
+    		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("register.fxml"));
+			Scene scene = new Scene(fxmlLoader.load());
+	        stage.setScene(scene);
+	        stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- youtube/src/main/java/module-info.java
+++ youtube/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module br.com.adrianbatista.youtube {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
     opens br.com.adrianbatista.youtube to javafx.fxml;
     exports br.com.adrianbatista.youtube;
