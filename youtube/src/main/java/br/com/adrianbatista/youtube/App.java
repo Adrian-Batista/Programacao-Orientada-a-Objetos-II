ff1fb2ef932e429cbbf788aeaa801ba6b0301b29
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-03-04 20:40:06 -0300

    App

--- youtube/src/main/java/br/com/adrianbatista/youtube/App.java
+++ youtube/src/main/java/br/com/adrianbatista/youtube/App.java
@@ -29,76 +29,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage){
-    	
-    	List<String> users = consultAPI();
-    	consumeAPI(users);
-    	
     	this.stage = stage;
         stage.setScene(FXMLUtil.loadScene("login"));
         this.centralizar();
         this.changeResizable();
-        
         stage.show();
-    }
-    
-    //-------------------------------------------------------
-    
-    private void consumeAPI(List<String>users) {
-    	for(int lineIndex = 0; lineIndex < users.size(); lineIndex++ ) {
-    		String line = users.get(lineIndex);
-    		if(line.contains("username")) {
-    			// processamos o nome de Usuario
-    			String username = processJSONLine(line);
-    			System.out.println(username);
-    			
-    			// vamos para a proxima linha
-    			lineIndex++;
-    			line = users.get(lineIndex);
-    			
-    			// processamos o password
-    			String password = processJSONLine(line);
-    			System.out.println(password);
-    			
-    			User user = new User(username, password);
-    			
-    			EntityManager em = UtilDB.getEntityManager();
-    			em.getTransaction().begin();
-    			em.persist(user);
-    			em.getTransaction().commit();
-    			em.close();
-    			UtilDB.closeConn();
-    		}
-    	}
-    }
-
-	private String processJSONLine(String line) {
-		String[] dividedLine = line.split(":");
-		String username = dividedLine[1];
-		username = username.replace(",", " ");
-		username = username.replace("\"", " ");
-		username = username.trim();
-		return username;
-	}
-    
-    private List<String> consultAPI(){
-    	List<String> result = new ArrayList<>();
-    	try {
-    		URL url = new URL("https://lucasbueno.com.br/steam.json");
-    		URLConnection uc = url.openConnection();
-    		InputStreamReader input = new InputStreamReader(uc.getInputStream());
-    		BufferedReader in = new BufferedReader(input);
-    		String inputLine;
-    		
-    		while((inputLine = in.readLine()) != null)
-    			result.add(inputLine);
-    		
-    		in.close();
-    	}catch(Exception e) {
-    		Alert alert = ExceptionUtil.error("Erro", "Erro ao consumir a API!", "Erro ao consumir a API!", e);
-    		alert.showAndWait();
-    		
-    	}
-    	return result;
     }
 
     public static void setRoot(String fxml) {
@@ -110,8 +45,7 @@ public class App extends Application {
     		stage.setResizable(true);
     	}else {
     		stage.setResizable(false);
-    	}
-    		
+    	}	
     }
     
     public static void centralizar() {
