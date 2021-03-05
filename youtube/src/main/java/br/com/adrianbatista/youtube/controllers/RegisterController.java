60bed057f1ca1ec80f8f05458e6c2eedf0fbd19a
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-03-05 19:21:59 -0300

    Register exceptions

--- youtube/src/main/java/br/com/adrianbatista/youtube/controllers/RegisterController.java
+++ youtube/src/main/java/br/com/adrianbatista/youtube/controllers/RegisterController.java
@@ -1,13 +1,52 @@
 package br.com.adrianbatista.youtube.controllers;
 
+import br.com.adrianbatista.youtube.ExceptionUtil;
+import br.com.adrianbatista.youtube.db.UserDAO;
+import br.com.adrianbatista.youtube.entities.User;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class RegisterController {
 	
 	@FXML 
 	private Button btnReturn;
+	
+	@FXML
+	private TextField txtEmail;
+	
+	@FXML
+	private PasswordField txtPassword;
+	
+	
+	public void register() {
+		String email = txtEmail.getText();
+		String password = txtPassword.getText();
+		
+		if(email.isBlank()) {
+			Alert alert = ExceptionUtil.error("Erro!", "ERRO Digite o e-mail!", "", null);	
+			alert.showAndWait();
+			return;
+		}
+		
+		if(password.isBlank()) {
+			Alert alert = ExceptionUtil.error("Erro!", "ERRO Digite a senha!", "", null);	
+			alert.showAndWait();
+			return;
+		}
+		
+		User u = new UserDAO().get(email);
+		if(u != null) {
+			Alert alert = ExceptionUtil.error("Erro!", "ERRO Email já em uso!", "", null);	
+			alert.showAndWait();
+			return;
+		}
+		new UserDAO().persist(new User(email, password));
+		
+	}
 	
 	@FXML
 	private void voltar(){
